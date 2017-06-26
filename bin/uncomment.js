8f9070d2becc85858a70898cbb5548b993bb622a
Nick Freear <nfreear@yahoo.co.uk>
2017-06-26 21:33:02 +0100

    First commit of 'uncomment-cli'.

---
+++ bin/uncomment.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+/*!
+  CLI. Strip most comments from Javascript, piped via stdin.
+
+  © Nick Freear, 13-June-2017 | License: MIT.
+*/
+
+const uncomment = require('uncomment');
+
+process.stderr.write('uncomment-cli\n');
+process.stdin.setEncoding('utf8');
+
+// https://gist.github.com/mhart/2585671
+process.stdin.resume();
+process.stdin.on('data', function (data) {
+  // .
+  // Multi-line: https://stackoverflow.com/questions/3577767/javascript-comment-stripper
+  // var strip = data.replace(/\/\*[^!](.|[\r\n])*?\*\//g, ''); // [^\/]
+  var strip = uncomment(data, {
+    // removeEmptyLines: true
+  });
+
+  // Single line, except "//# MAP" lines.
+  // strip = strip.replace(/\/\/ .*/gm, '');
+
+  process.stdout.write(strip);
+  // process.stderr.write('>Data:' + strip);
+});
+
+process.stdout.on('error', function (err) {
+  if (err.code === 'EPIPE') return process.exit();
+  process.emit('error', err);
+});
